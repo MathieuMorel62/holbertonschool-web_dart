60c92eb8e73e6052e7cdde47f2a61afb5fe28f56
Cirrus CI <support@cirruslabs.org>
2024-06-26 13:06:30 +0000

    Add asynchronous function to calculate total price of user orders

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,27 @@
+import '4-util.dart';
+import 'dart:convert';
+
+
+Future<String> calculateTotal() async {
+  try {
+    // Récupérer les données de l'utilisateur
+    String userData = await fetchUserData();
+    Map<String, dynamic> userMap = jsonDecode(userData);
+    String id = userMap['id'];
+
+    // Récupérer les commandes de l'utilisateur
+    String orders = await fetchUserOrders(id);
+    List<dynamic> orderList = jsonDecode(orders);
+
+    double totalPrice = 0.0;
+
+    // Calculer le prix total des commandes
+    for (var order in orderList) {
+      String productPrice = await fetchProductPrice(order);
+      totalPrice += jsonDecode(productPrice);
+    }
+    return '${totalPrice}';
+  } catch (error) {
+    return 'error caught: $error';
+  }
+}
